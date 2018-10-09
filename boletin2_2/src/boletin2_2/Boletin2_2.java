dab4e9e41fb72ba2fba8031f46f922b1e7c4dd0d
Oracle <oracle@nautilus1.danielcastelao.org>
2018-10-09 11:09:40 +0200

    resultado graos fahrenheit e kelvin

--- boletin2_2/src/boletin2_2/Boletin2_2.java
+++ boletin2_2/src/boletin2_2/Boletin2_2.java
@@ -11,6 +11,10 @@ public class Boletin2_2 {
        Scanner ler= new Scanner (System.in);
        double graosC, graosF, graosK;
        System.out.println("Introduce graos centígrados");
+       graosC=ler.nextDouble();
+       graosF= 32+(9*graosC/5);
+       graosK= graosC+273.15f;
+       System.out.println("Graos Fahrenheit: "+graosF +".\nGraos Kelvin: "+graosK+".");
     }
     
 }
