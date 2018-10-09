0c51d973155ae7169f2e637062b58b83ad0f7e2b
Oracle <oracle@nautilus1.danielcastelao.org>
2018-10-09 12:11:16 +0200

    calculo

--- boletin2_3/src/boletin2_3/Boletin2_3.java
+++ boletin2_3/src/boletin2_3/Boletin2_3.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin2_3;
 
-/**
- *
- * @author oracle
- */
+import java.util.Scanner;
+
+
 public class Boletin2_3 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner ler= new Scanner(System.in);
+        int cantTotal, eur100, num1, eur20, num2, eur5, num3, eur1, num4; 
+        System.out.println("Introduce número de billetes de 100€:");
+        eur100=ler.nextInt();
+        num1=eur100*100;
+        System.out.println("Introduce número de billetes de 20€:");
+        eur20=ler.nextInt();
+        num2=eur20*20;
+        System.out.println("Introduce número de billetes de 5€:");
+        eur5=ler.nextInt();
+        num3=eur5*5;
+        System.out.println("Introduce número de monedas de 1€:");
+        eur1=ler.nextInt();
+        num4=eur1*1;
+        cantTotal=num1+num2+num3+num4;
+        System.out.println("Cantidade total:"+cantTotal+".");
+      
+        }
+        
     }
     
-}
