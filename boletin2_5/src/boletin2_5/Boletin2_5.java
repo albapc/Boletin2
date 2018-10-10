7b47de8aad1cdf00a733a37fa7e06bf3b54c959c
Oracle <oracle@nautilus1.danielcastelao.org>
2018-10-10 12:13:40 +0200

    calcular sueldo bruto y liquido

--- boletin2_5/src/boletin2_5/Boletin2_5.java
+++ boletin2_5/src/boletin2_5/Boletin2_5.java
@@ -1,21 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin2_5;
 
-/**
- *
- * @author oracle
- */
+import java.util.Scanner;
+
+
 public class Boletin2_5 {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+     Scanner ler= new Scanner(System.in); 
+    
+     System.out.println("Introduzca el sueldo fijo:");//sueldo bruto
+     double sueldoFijo=ler.nextDouble();
+     
+     System.out.println("Introduzca el importe total de ventas:");//comision
+     double importeVentas=ler.nextDouble();
+     double comision=(importeVentas*5)/100;
+     
+     System.out.println("Introduzca kilómetros realizados:");//kilometraje
+        double kmTotales= ler.nextDouble();
+        double kilometraje= kmTotales*2;
+     
+     System.out.println("Introduzca número de días de desplazamiento:");//dietas
+        double diasDespl=ler.nextDouble();
+        double dietas=diasDespl*30;
+     
+     double sueldoBruto= sueldoFijo+comision+kilometraje+dietas;
+     
+     //calculo sueldo liquido
+     
+     double irpf=(sueldoBruto*18)/100;
+     int segSoc=36;
+     double sueldoLiq= sueldoBruto-(irpf+segSoc);
+     
+     System.out.println("Sueldo bruto a percibir: "+sueldoBruto+"\nSueldo líquido a percibir: "+sueldoLiq);
     }
     
 }
